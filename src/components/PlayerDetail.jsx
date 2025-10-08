a7ca553b4aba749255c174f8ea947221c11285d1
MrStegmann <pjstegmann@outlook.es>
2025-10-08 11:56:19 +0200

    fixed playerdetail

--- src/components/PlayerDetail.jsx
+++ src/components/PlayerDetail.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { ImageBackground, Text, View } from "react-native";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
 import useDB from "../hooks/useDB";
 import jersey from "../../assets/jersey.png";
 import { timeFormat } from "../helpers/timeFormat";
 import StatsEnums from "../Enums/StatsEnums";
+import ModalInfo from "../framework/ModalInfo";
 
 const STATS_CONFIG = [
   StatsEnums.minutes,
@@ -24,20 +25,51 @@ const STATS_CONFIG = [
   StatsEnums.per,
 ];
 
-const StatsByGame = React.memo(({ info }) => {
+const GameStats = React.memo(({ stats }) => {
+  if (!stats) return null;
   return (
-    <View className="w-full flex flex-col">
-      <Text className="text-danish-white text-xs">{`${info.round} - ${info.date} ${info.opponent}`}</Text>
-      <View className="w-full flex flex-row justify-between flex-wrap gap-x-5">
+    <View
+      className={`w-full px-5 mt-5 flex flex-col justify-center items-center`}
+    >
+      <View className="w-14 border border-danish-red rounded-lg p-1 flex flex-col items-center justify-center">
+        <Text className="text-danish-white text-sm">Val</Text>
+        <Text className="text-danish-white">{`${stats.val || 0}`}</Text>
+      </View>
+      <View className="w-full flex flex-row flex-wrap justify-between items-center mt-5 gap-y-2">
         {STATS_CONFIG.map((stat) =>
           stat === StatsEnums.minutes ? (
-            <Text className="text-danish-white text-xs">{`Mins: ${timeFormat(info[stat]) || 0}`}</Text>
+            <View
+              key={stat}
+              className="w-14 border border-danish-red rounded-lg p-1 flex flex-col items-center justify-center"
+            >
+              <Text className="text-danish-white text-sm">Mins</Text>
+              <Text className="text-danish-white">{`${timeFormat(stats[`${stat}`]) || 0}`}</Text>
+            </View>
           ) : (
-            <Text className="text-danish-white capitalize text-xs">{`${stat}: ${info[stat] || 0}`}</Text>
+            <View
+              key={stat}
+              className="w-14 border border-danish-red rounded-lg p-1 flex flex-col items-center justify-center"
+            >
+              <Text className="text-danish-white capitalize text-sm">
+                {stat}
+              </Text>
+              <Text className="text-danish-white">{`${stats[`${stat}`] || 0}`}</Text>
+            </View>
           )
         )}
       </View>
     </View>
+  );
+});
+
+const StatsByGame = React.memo(({ info, onSelect }) => {
+  return (
+    <TouchableOpacity
+      className="flex items-center bg-danish-dark-gray rounded-lg shadow-lg shadow-danish-red p-1 border border-danish-red hover:border-danish-gold active:border-danish-gold hover:bg-danish-red active:bg-danish-red my-2"
+      onPress={() => onSelect(info)}
+    >
+      <Text className="text-danish-white text-xs">{`${info.round} - ${info.date} ${info.opponent}`}</Text>
+    </TouchableOpacity>
   );
 });
 
@@ -91,6 +123,7 @@ const PlayerDetail = ({ data }) => {
   const [playerAllStats, setPlayerAllStats] = useState([]);
   const [stats, setStats] = useState([]);
   const [gameStats, setGameStats] = useState([]);
+  const [gameStatSelected, setGameStatSelected] = useState(null);
   const [totalTrainings, setTotalTrainings] = useState(0);
   const [totalTrainingsAssis, setTotalTrainingsAssis] = useState(0);
 
@@ -115,6 +148,12 @@ const PlayerDetail = ({ data }) => {
 
   return (
     <View className="w-full h-full flex flex-col items-center">
+      <ModalInfo
+        title={`${gameStatSelected?.round} - ${gameStatSelected?.date} ${gameStatSelected?.opponent}`}
+        information={<GameStats stats={gameStatSelected} />}
+        visible={Boolean(gameStatSelected)}
+        onClose={() => setGameStatSelected(null)}
+      />
       <View className="w-full flex flex-col px-5">
         <View className="w-full flex flex-row gap-2 h-12">
           <ImageBackground
@@ -161,7 +200,11 @@ const PlayerDetail = ({ data }) => {
       )}
       <View className="w-full mt-2 mb-20">
         {gameStats.map((info) => (
-          <StatsByGame key={info.date} info={info} />
+          <StatsByGame
+            key={info.date}
+            info={info}
+            onSelect={setGameStatSelected}
+          />
         ))}
       </View>
     </View>
